cc5e2dab46bcf89044a82c620a52bf814bf3b8b7
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-04-07 15:14:54 +0900

    [Bronze III] Title: 학점계산, Time: 92 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const score = fs.readFileSync(0).toString().trim();
+
+switch (score) {
+  case "A+":
+    console.log(4.3);
+    break;
+  case "A0":
+    console.log("4.0");
+    break;
+  case "A-":
+    console.log(3.7);
+    break;
+  case "B+":
+    console.log(3.3);
+    break;
+  case "B0":
+    console.log("3.0");
+    break;
+  case "B-":
+    console.log(2.7);
+    break;
+  case "C+":
+    console.log(2.3);
+    break;
+  case "C0":
+    console.log("2.0");
+    break;
+  case "C-":
+    console.log(1.7);
+    break;
+  case "D+":
+    console.log(1.3);
+    break;
+  case "D0":
+    console.log("1.0");
+    break;
+  case "D-":
+    console.log(0.7);
+    break;
+  default:
+    console.log("0.0");
+}
