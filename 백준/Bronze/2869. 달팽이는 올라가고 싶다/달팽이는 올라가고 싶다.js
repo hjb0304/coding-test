3b1593b5851b023ef537f86d62e29a35da09b748
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-04-10 15:48:11 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.js
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.js
@@ -6,16 +6,11 @@ let b = parseInt(input[1]);
 let v = parseInt(input[2]);
 
 let height = 0;
-let day = 0;
 
 // 달팽이는 매일 a - b만큼 올라간다. 마지막 날은 b만큼 올라가지 않는다.
 // 전체 높이에서 마지막 날 올라가지 않는 높이(b)를 빼고 매일 올라가는 높이(a - b)로 나눈다.
-// 나머지가 0이 아닌 경우 하루 더 올라가야 함 => day+1
-if ((v - b) % (a - b)) {
-  day = Math.ceil((v - b) / (a - b));
-} else {
-  day = (v - b) / (a - b);
-}
+// 나머지가 0이 아닌 경우 하루 더 올라가야 함 => 올림
+let day = Math.ceil((v - b) / (a - b));
 
 // 반복문: 시간초과
 // while (height < v) {
