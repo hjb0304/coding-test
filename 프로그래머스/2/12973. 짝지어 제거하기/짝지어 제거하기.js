0542e8e1a06736fe5bafdbfbd442a5460ec60904
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-04-22 01:04:20 +0900

    [level 2] Title: 짝지어 제거하기, Time: 49.78 ms, Memory: 43.5 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -7,20 +7,13 @@ function solution(s) {
         if(arr[arr.length - 1] === s[i]) {
             // 배열의 마지막 알파벳 제거
             arr.pop();
-            continue;
         }
-        // 배열의 i번째, i+1번째 알파벳이 다를 경우
-        if(s[i] !== s[i + 1]) {
+        // 짝지어지지 않을 경우
+        else {
             // 배열에 짝 없는 알파벳 추가
             arr.push(s[i])
-        }
-        // 배열의 i번째, i+1번째 알파벳이 같을 경우
-        if(s[i] === s[i + 1]) {
-            // 다음 반복 건너뜀
-            i++;
         }
     }
     
     return arr.length === 0 ? 1 : 0;
-    // return arr;
 }
