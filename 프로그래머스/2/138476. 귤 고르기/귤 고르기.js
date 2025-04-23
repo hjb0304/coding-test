2327f67209ca339665b3f391e3639167f5c66358
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-04-23 21:04:36 +0900

    [level 2] Title: 귤 고르기, Time: 403.88 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -5,33 +5,28 @@ function solution(k, tangerine) {
     
     // 각 크기의 귤의 개수를 담은 객체
     for(let tan of tangerine) {
-        if(obj.hasOwnProperty(tan)) {
-            obj[tan]++;
-        } else {
-            obj[tan] = 1;
-        }
+        // if(obj.hasOwnProperty(tan)) {
+        //     obj[tan]++;
+        // } else {
+        //     obj[tan] = 1;
+        // }
+        obj[tan] = (obj[tan] || 0) + 1;
     }
     
-    // 객체를 배열로 변환
-    const arr = Object.entries(obj)
-    
-    // 배열을 귤 개수가 많은 순으로 정렬
-    arr.sort((a, b) => b[1] - a[1]);
+    // 귤 개수 배열을 내림차순으로 정렬
+    const arr = Object.values(obj).sort((a, b) => b - a);
     
     // 상자에 담은 귤이 k개가 될 때까지 담음
-    let i = 0;
-    while(choose < k) {
-        let num = arr[i][1];
-        // 귤을 더한 총합이 k 이하일 경우 그 크기의 귤 전체를 담음
-        if(choose + num <= k) {
-            choose += num;
-            // 총합이 k를 초과할 경우 총합은 바로 k가 됨
-        } else {
-            choose = k;
-        }
+    for(tan of arr) {
         // 한 종류의 귤을 담을 때마다 종류의 수 증가
         count++;
-        i++;
+        // 귤을 더한 총합이 k보다 작을 경우 그 크기의 귤 전체를 담음
+        if(choose + tan < k) {
+            choose += tan;
+            // 총합이 k 이상일 경우 담기 중단
+        } else {
+            break;
+        }
     }
     
     return count;
