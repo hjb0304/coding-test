2a50a3c0cac8c8829130a206ac34a377a98bd22d
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-06-02 23:18:17 +0900

    [level 2] Title: 프로세스, Time: 0.47 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,31 +1,24 @@
 function solution(priorities, location) {
     let count = 0;
-    // 각 프로세스의 중요도를 나타내는 객체
-    const processes = {};
+    // 각 프로세스의 번호와 우선순위를 나타내는 객체 배열
+    const processes = [];
     
+    // 프로세스 번호와 중요도 넣기 
     priorities.forEach((priority, i) => {
-        processes[i] = priority;
-    })
+        processes[i] = {index: i, priority: priority};
+    });
     
-    // 각 프로세스 배열
-    const processArr = Object.keys(processes);
-    // 우선순위를 구할 프로세스
-    const answerProcess = processArr[location];
-    // 우선순위 큰 순서대로 정렬
-    priorities.sort((a, b) => b - a);
-    
-    while(processArr.length > 0) {
+    while(processes.length > 0) {
         // 프로세스 꺼내기
-        let currentProcess = processArr.shift();
-        // 우선순위의 최댓값이 현재 프로세스의 우선순위보다 높을 경우 다시 넣기
-        if(priorities[0] > processes[currentProcess]) {
-           processArr.push(currentProcess);
+        let currentProcess = processes.shift();
+        // 남은 우선순위 중 하나라도 현재 프로세스의 우선순위보다 클 경우 다시 넣기
+        if(processes.some((process) => process.priority > currentProcess.priority)) {
+           processes.push(currentProcess);
             // 더 높은 우선순위가 없다면 실행 후 순서 증가
            } else {
-               priorities.shift();
                count++;
-               // 방금 실행된 프로세스가 해당 프로세스일 경우 반복문 종료
-               if(currentProcess === answerProcess) {
+               // 방금 실행된 프로세스 번호가 해당 프로세스 번호일 경우 반복문 종료
+               if(currentProcess.index === location) {
                    break;
                }
            }
