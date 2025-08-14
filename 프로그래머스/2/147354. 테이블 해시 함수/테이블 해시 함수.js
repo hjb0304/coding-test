c07e6e635ae07651542c5b1b820b127ad038d4cb
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-08-14 23:40:28 +0900

    [level 2] Title: 테이블 해시 함수, Time: 18.63 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
@@ -5,7 +5,7 @@ function solution(data, col, row_begin, row_end) {
     // 값이 같으면 첫번째 컬럼의 값을 기준으로 내림차순 정렬
     data.sort((a, b) => a[col - 1] - b[col - 1] || b[0] - a[0]);
     
-    // S_i 누적
+    // 모든 S_i를 배열에 추가
     data.forEach((tuple, i) => {
         if(i >= row_begin - 1 && i <= row_end - 1) {
             let sum = 0;
@@ -16,10 +16,6 @@ function solution(data, col, row_begin, row_end) {
         }
     })
     
-    let result = answer[0];
-    for(let i = 1; i < answer.length; i++) {
-        result = result^answer[i];
-    }
-    
-    return result;
+    // XOR 연산
+    return answer.reduce((acc, cur) => acc^cur);
 }
