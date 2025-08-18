b4075d54c9855b83b073e2cda81d3cd306a91342
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-08-19 01:34:21 +0900

    [level 3] Title: 입국심사, Time: 120.68 ms, Memory: 43.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -0,0 +1,20 @@
+function solution(n, times) {
+    let right = Math.max(...times) * n;
+    let left = Math.min(...times);
+    
+    while(left <= right) {
+        let mid = Math.floor((right + left) / 2);
+        
+        // 총 시간 동안 각 심사관이 심사할 수 있는 사람의 수 총합 구하기
+        const people = times.reduce((acc, cur) => acc + Math.floor(mid / cur), 0);
+        
+        if(people < n) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
+    
+    return left;
+}
