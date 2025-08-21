d2d3987886c3b9f623ce3f693189fdc4e56addbf
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-08-21 15:43:36 +0900

    [level 2] Title: 기능개발, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,30 +1,48 @@
 function solution(progresses, speeds) {
-    var answer = [];
+//     var answer = [];
     
-    // 기능이 하나도 남지 않을 때까지 반복
-    while(progresses.length) {
-        let count = 0;
-        // 첫번째 작업이 끝날 때까지 남은 일수
-        let day = Math.ceil((100 - progresses[0]) / speeds[0]); 
-        // 목록에서 제거 후 개수 증가
-        progresses.shift();
-        speeds.shift();
-        count++;
-        // 남은 작업들이 끝났는지 확인
-        progresses = progresses.map((progress, i) => (progress + day * speeds[i]));
+//     // 기능이 하나도 남지 않을 때까지 반복
+//     while(progresses.length) {
+//         let count = 0;
+//         // 첫번째 작업이 끝날 때까지 남은 일수
+//         let day = Math.ceil((100 - progresses[0]) / speeds[0]); 
+//         // 목록에서 제거 후 개수 증가
+//         progresses.shift();
+//         speeds.shift();
+//         count++;
+//         // 남은 작업들이 끝났는지 확인
+//         progresses = progresses.map((progress, i) => (progress + day * speeds[i]));
 
-        while(progresses.length) {
-            // 끝났다면 제거 후 개수 증가
-            if(progresses[0] >= 100) {
-                progresses.shift();
-                speeds.shift();
-                count++;
-            } else {
-                break;
-            }
-        }
-        answer.push(count);
-    }
+//         while(progresses.length) {
+//             // 끝났다면 제거 후 개수 증가
+//             if(progresses[0] >= 100) {
+//                 progresses.shift();
+//                 speeds.shift();
+//                 count++;
+//             } else {
+//                 break;
+//             }
+//         }
+//         answer.push(count);
+//     }
     
-    return answer;
+//     return answer;
+      const answer = [];
+
+  progresses = progresses.map((progress, i) => Math.ceil((100 - progress) / speeds[i]));
+
+  while (progresses.length) {
+    const first = progresses.shift();
+    let sum = 1;
+
+    progresses = progresses.map((progress) => progress - first);
+
+    while (progresses[0] <= 0) {
+      progresses.shift();
+      sum++;
+    }
+    answer.push(sum);
+  }
+
+  return answer;
 }
