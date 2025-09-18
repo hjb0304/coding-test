9c2ba753b6a4ac11f6e1b8f4ef2203d857ee42ae
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-09-19 00:44:33 +0900

    [level 2] Title: 전화번호 목록, Time: 352.33 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,11 +1,26 @@
 function solution(phone_book) {
     phone_book.sort();
     
-    for(let i = 1; i < phone_book.length; i++) {
-        // 바로 전 순서의 전화번호가 접두사인지 확인
-        if(phone_book[i].startsWith(phone_book[i - 1])) {
-            return false;
+    // for(let i = 1; i < phone_book.length; i++) {
+    //     // 바로 전 순서의 전화번호가 접두사인지 확인
+    //     if(phone_book[i].startsWith(phone_book[i - 1])) {
+    //         return false;
+    //     }
+    // }
+    
+    // 해시 풀이
+    const obj = new Set();
+    
+    for(let phone_number of phone_book) {
+        for(let i = 0; i < phone_number.length; i++) {
+            // 글자수별 접두사 만들기
+            const prefix = phone_number.slice(0, i + 1);
+            // set에 접두사가 있으면 false 반환
+            if(obj.has(prefix)) return false;
         }
+        
+        // 전화번호를 set에 추가
+        obj.add(phone_number);
     }
     
     return true;
