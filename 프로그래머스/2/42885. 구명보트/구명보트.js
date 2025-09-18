46faacd851de67a3f042c76ca4d66e3ab5c8179a
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-09-18 16:46:19 +0900

    [level 2] Title: 구명보트, Time: 33.42 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,17 +1,17 @@
 function solution(people, limit) {
-    var answer = 0;
+    let count = 0;
     
-    // 사람들 배열을 몸무게가 큰 순으로 정렬
+    // 몸무게 내림차순 정렬
     people.sort((a, b) => b - a);
     
-    // 남아있는 가장 몸무게가 적은 사람과 같이 탈 수 있다면 같이 타는 사람 제거 후 보트 개수 증가
-    // 혼자 타야 한다면 보트 개수만 증가
-    people.forEach((e) => {
-        if(e + people[people.length - 1] <= limit) {
+    for(let person of people) {
+        // 가장 무거운 사람 + 가장 가벼운 사람을 같이 태울 수 있다면
+        if(person + people[people.length - 1] <= limit) {
+            // 가벼운 사람 제거
             people.pop();
-        }
-        answer++;
-    })
-    
-    return answer;
+        } 
+        // 보트 수 증가
+        count++;
+    }
+    return count;
 }
