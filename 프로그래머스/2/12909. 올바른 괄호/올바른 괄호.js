5a8440c07dfad20ee8fa341916caad496f69d85f
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-09-19 15:05:17 +0900

    [level 2] Title: 올바른 괄호, Time: 5.51 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,20 +1,40 @@
 function solution(s) {
-    var answer = 0;
+    // var answer = 0;
     
-    // while문과 replaceAll()로 풀면 시간복잡도 두 배
-    for(let i = 0; i < s.length; i++) {
-        // 괄호 열면 +
-        if(s[i] === '(') {
-            answer++;
-            // 괄호 닫히면 -
+//     // while문과 replaceAll()로 풀면 시간복잡도 두 배
+//     for(let i = 0; i < s.length; i++) {
+//         // 괄호 열면 +
+//         if(s[i] === '(') {
+//             answer++;
+//             // 괄호 닫히면 -
+//         } else {
+//             answer--;
+//         }
+//         // 괄호 열기 전에 닫히면 음수가 됨
+//         if(answer < 0) {
+//             return false;
+//         }
+//     }
+
+//     return answer === 0 ?  true : false;
+    
+    // 스택 풀이
+    const arr = [...s];
+    const stack = [arr.shift()];
+    
+    for(let i = 0; i < arr.length; i++) {
+        // 괄호 꺼내기
+        const current = arr[i];
+        
+        // 스택의 마지막 괄호와 짝 맞는지 체크
+        if(stack[stack.length - 1] === '(' && current === ')') {
+            // 맞으면 마지막 괄호 빼기
+            stack.pop();
         } else {
-            answer--;
-        }
-        // 괄호 열기 전에 닫히면 음수가 됨
-        if(answer < 0) {
-            return false;
+            // 맞지 않으면 스택에 넣기
+            stack.push(current);
         }
     }
-
-    return answer === 0 ?  true : false;
+    
+    return stack.length ? false : true;  
 }
