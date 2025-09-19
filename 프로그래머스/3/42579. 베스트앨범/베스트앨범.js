3969c181671f11816fbf168d36edccaa098d41e3
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-09-19 11:17:49 +0900

    [level 3] Title: 베스트앨범, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,28 +1,31 @@
 function solution(genres, plays) {
-    var answer = [];
+    let answer = [];
     const obj = {};
-    // {'classic': {'music': [1, 2, 3], 'play': 0}}
+    // 'classic': {songs: [0, 2], total: 650}
     
-    // 장르별 노래 분류
+    // 노래 장르별 노래의 고유 번호, 총 재생횟수 객체에 넣기
     genres.forEach((genre, i) => {
         if(obj[genre]) {
-            obj[genre].music.push(i)
-            obj[genre].play += plays[i];
+            obj[genre].songs.push(i);
+            obj[genre].total += plays[i];
         } else {
-            obj[genre] = {};
-            obj[genre].music = [i];
-            obj[genre].play = plays[i];
+            // 장르의 객체가 생성되지 않은 경우 정의해줘야 함
+            obj[genre] = {songs: [i], total: plays[i]};
         }
     });
     
-    // 많이 재생된 장르순으로 정렬
-    const set = [...new Set(genres)];
-    set.sort((a, b) => obj[b].play - obj[a].play);    
+    // 많이 재생된 장르 순서대로 정렬    
+    const sortedGenres = [...new Set(genres)];
+    sortedGenres.sort((a, b) => obj[b].total - obj[a].total);
     
-    // 많이 재생된 노래순으로 정렬해서 2개 선택
-    for(let genre of set) {
-        const sorted = obj[genre].music.sort((a, b) => plays[b] - plays[a]).slice(0, 2);
-        answer.push(...sorted);
+    for(genre of sortedGenres) {
+        // 장르별 노래 많이 재생된 순서대로 정렬 (이미 고유 번호 낮은 순서대로 정렬되어 있음)
+        let sortedSongs = obj[genre].songs;
+        sortedSongs.sort((a, b) => plays[b] - plays[a]);
+        // 2개 선택
+        sortedSongs = sortedSongs.slice(0, 2);
+        
+        answer.push(...sortedSongs);
     }
     
     return answer;
