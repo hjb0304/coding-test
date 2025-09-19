6487d9d721b2a03302de5759d82bcf02c2c4cfe3
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-09-19 18:38:15 +0900

    [level 2] Title: H-Index, Time: 0.51 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,5 +1,5 @@
 function solution(citations) {
-    var answer = 0;
+    // var answer = 0;
     
     // for(let i = citations.length; i > 0; i--) {
     //     let cited = 0;
@@ -13,12 +13,26 @@ function solution(citations) {
     //         break;
     //     }
     // }
-    let max = citations.length;
-    while(max > 0) {
-        const cited = citations.filter(citation => citation >= max).length;
-        if(cited >= max) return max;
-        max--;
+    
+//     let max = citations.length;
+//     while(max > 0) {
+//         const cited = citations.filter(citation => citation >= max).length;
+//         if(cited >= max) return max;
+//         max--;
+//     }
+    
+//     return max;
+    
+    // 정렬 풀이
+    let h = 0;
+    
+    // 내림차순 정렬
+    citations.sort((a, b) => b - a);
+    
+    // h번째 논문의 인용 횟수가 논문 개수 이상이면 h 증가
+    while(h + 1 <= citations[h]) {
+        h++;
     }
     
-    return max;
+    return h;
 }
