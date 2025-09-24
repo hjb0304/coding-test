fc4dc625b947c3911ed1c4fd4c67c7007174633a
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-09-24 23:02:13 +0900

    Time: 6 ms (50.86%), Space: 57.3 MB (72.34%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.js
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,7 +3,20 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+    // 소문자로 변환, 알파벳과 숫자 이외의 문자 제거
     const converted = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
-console.log(converted)
-    return converted === [...converted].reverse().join('');
+
+    // 투포인터: 문자열의 양끝을 포인터로 잡기
+    let start = 0;
+    let end = converted.length - 1;
+
+    // 두 포인터가 교차하기 전까지 비교
+    while(start < end) {
+        if(converted[start] !== converted[end]) return false;
+        start++;
+        end--;
+    }
+
+    return true;
+    // return converted === [...converted].reverse().join('');
 };
