4f43458145bf9ee380a912a3875ab600ec211e19
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-09-25 23:25:48 +0900

    Time: 2 ms (80.11%), Space: 54.6 MB (71.79%) - LeetHub

--- LeetCode/Medium/0120-triangle/0120-triangle.js
+++ LeetCode/Medium/0120-triangle/0120-triangle.js
@@ -5,14 +5,17 @@
 var minimumTotal = function(triangle) {
     for(let i = 1; i < triangle.length; i++) {
         for(let j = 0; j < triangle[i].length; j++) {
+            // 두번째 행의 경우 첫번째 숫자 누적
             if(i === 1) {
                 triangle[i][j] += triangle[0][0];
                 continue;
             }
+            // 첫번째 열의 경우 윗줄 첫번째 열 숫자 누적
             if(j === 0) {
                 triangle[i][j] += triangle[i - 1][j];
                 continue;
             }
+            // 마지막 열의 경우 윗줄 마지막 열 숫자 누적
             if(j === triangle[i].length - 1) {
                 triangle[i][j] += triangle[i - 1][j - 1];
                 continue;
@@ -23,5 +26,6 @@ var minimumTotal = function(triangle) {
         }
     }
 
+    // 맨 밑줄에서 가장 작은 수 반환
     return Math.min(...triangle[triangle.length - 1]);
 };
