732f2baa07a00b53823899cdc1bba0d8f08fcafe
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-09-25 23:27:19 +0900

    Time: 2 ms (80.11%), Space: 57.4 MB (15.37%) - LeetHub

---
+++ LeetCode/Medium/0120-triangle/0120-triangle-02.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[][]} triangle
+ * @return {number}
+ */
+var minimumTotal = function(triangle) {
+    // for(let i = 1; i < triangle.length; i++) {
+    //     for(let j = 0; j < triangle[i].length; j++) {
+    //         // 두번째 행의 경우 첫번째 숫자 누적
+    //         if(i === 1) {
+    //             triangle[i][j] += triangle[0][0];
+    //             continue;
+    //         }
+    //         // 첫번째 열의 경우 윗줄 첫번째 열 숫자 누적
+    //         if(j === 0) {
+    //             triangle[i][j] += triangle[i - 1][j];
+    //             continue;
+    //         }
+    //         // 마지막 열의 경우 윗줄 마지막 열 숫자 누적
+    //         if(j === triangle[i].length - 1) {
+    //             triangle[i][j] += triangle[i - 1][j - 1];
+    //             continue;
+    //         }
+    //         // 윗줄 j와 j-1번째 중 더 작은 숫자 선택 후 누적
+    //         const min = Math.min(triangle[i - 1][j - 1], triangle[i - 1][j]);
+    //         triangle[i][j] += min;
+    //     }
+    // }
+
+    // // 맨 밑줄에서 가장 작은 수 반환
+    // return Math.min(...triangle[triangle.length - 1]);
+
+    // 다른 풀이: 맨 밑줄부터 세기 (경우의 수 따지지 않아도 됨)
+    const row = triangle.length;
+
+    for(let i = row - 2; i >= 0; i--) {
+        for(let j = 0; j < triangle[i].length; j++) {
+            // 밑줄의 j, j+1 중 작은 숫자 누적
+            const min = Math.min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+            triangle[i][j] += min;
+        }
+    }
+
+    return triangle[0][0];
+};
