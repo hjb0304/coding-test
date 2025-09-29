f3396eecb2255be076f347a7260e37fdec5bc121
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-09-29 22:04:40 +0900

    Time: 4 ms (90.48%), Space: 63 MB (95.54%) - LeetHub

--- LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.js
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.js
@@ -3,14 +3,15 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
+    // 새로 구성하는 숫자
     let result = 0;
     let copyx = x;
 
     // 10으로 차례차례 나누기
     while (copyx > 0) {
+        // result를 첫째자리부터 채우고 그 다음 자리를 더함
         result = result * 10 + copyx % 10;
         copyx = Math.floor(copyx / 10);
-        console.log(result, copyx)
     }
 
     return result === x;
