658f1d45fc6fa9c68edaa93afedc1abb41a76073
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-10-03 21:48:40 +0900

    Time: 2 ms (72.62%), Space: 57.4 MB (25.1%) - LeetHub

--- LeetCode/Medium/0071-simplify-path/0071-simplify-path.js
+++ LeetCode/Medium/0071-simplify-path/0071-simplify-path.js
@@ -6,8 +6,6 @@ var simplifyPath = function (path) {
     const answer = [];
     const arr = path.split('/');
 
-    if (path === '/') return "/";
-
     for (let str of arr) {
         // ..이면 마지막 요소 제거
         if (str === '..') {
@@ -17,6 +15,6 @@ var simplifyPath = function (path) {
         // .는 빼고 스택에 넣기
         if (str !== '.' && str !== '') answer.push(str);
     }
-    console.log(answer)
+
     return !answer.length ? '/' : answer.map((str) => '/' + str).join('');
 };
