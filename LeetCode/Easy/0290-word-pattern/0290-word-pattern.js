c3ecfb7039f247cd5fd09a4b7455807f1f064dea
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-10-10 00:24:05 +0900

    Time: 0 ms (100%), Space: 53.7 MB (55.45%) - LeetHub

---
+++ LeetCode/Easy/0290-word-pattern/0290-word-pattern.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+    const dic = {};
+    const arr = s.split(' ');
+
+    // 단어와 알파벳 개수가 다르면 false
+    if(pattern.length !== arr.length) return false;
+
+    for(let i = 0; i < pattern.length; i++) {
+        const cha = pattern[i];
+        // 사전의 key나 value가 이미 있고 현재 단어와 다르면 false
+        if((dic[cha] || Object.values(dic).includes(arr[i])) && arr[i] !== dic[cha]) return false;
+        // 사전에 없거나 현재 단어와 같으면 사전에 추가
+        dic[cha] = arr[i];
+    }
+
+    return true;
+};
