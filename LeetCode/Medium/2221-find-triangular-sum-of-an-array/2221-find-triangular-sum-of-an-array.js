642d3f4e3a5f7adc31d80161a7482551229131a6
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-10-09 23:59:01 +0900

    Time: 38 ms (89.56%), Space: 56.1 MB (72.93%) - LeetHub

--- LeetCode/Medium/2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.js
+++ LeetCode/Medium/2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var triangularSum = function(nums) {
-    // 숫자가 하나 남을 때까지 반복
+    // 숫자가 하나 남을 때까지 반복     
     while(nums.length > 1) {
         for(let i = 0; i < nums.length - 1; i++) {
             const newNum = (nums[i] + nums[i + 1]) % 10;
