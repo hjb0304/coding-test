d5f67acbc73a8ed31d895b40a92bf3d3ea28341e
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-10-12 23:58:19 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.js
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.js
@@ -4,5 +4,17 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    return [...s].sort().join('') === [...t].sort().join('');
+    // 정렬 풀이
+    // return [...s].sort().join('') === [...t].sort().join('');
+
+    // 해시 풀이
+    const count = {};
+
+    for(let i = 0; i < s.length; i++) {
+        count[s[i]] = (count[s[i]] || 0) + 1;
+        count[t[i]] = (count[t[i]] || 0) - 1;
+    }
+
+    // 모든 값이 0이어야 아나그램
+    return Object.values(count).every(item => item === 0);
 };
