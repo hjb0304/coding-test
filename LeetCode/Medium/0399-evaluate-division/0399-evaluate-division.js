685d3d18ec8281fb78ffd4c295d3fc44cc79d14e
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-10-14 14:12:30 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Medium/0399-evaluate-division/0399-evaluate-division.js
@@ -0,0 +1,49 @@
+/**
+ * @param {string[][]} equations
+ * @param {number[]} values
+ * @param {string[][]} queries
+ * @return {number[]}
+ */
+var calcEquation = function (equations, values, queries) {
+    const map = new Map();
+    const answer = [];
+
+    // 각 알파벳마다 나눗셈 결과 저장
+    for (let i = 0; i < equations.length; i++) {
+        const [cha1, cha2] = equations[i];
+        if (!map.has(cha1)) map.set(cha1, new Map());
+        map.get(cha1).set(cha2, values[i]);
+        if (!map.has(cha2)) map.set(cha2, new Map());
+        map.get(cha2).set(cha1, 1 / values[i]);
+    }
+
+    // 나눗셈 결과 찾기
+    function dfs(start, end, result, visited) {
+        visited.add(start);
+        if (map.get(start).has(end)) return result * map.get(start).get(end);
+
+        for (let cha of map.get(start).keys()) {
+            if (!visited.has(cha)) {
+                const next = dfs(cha, end, result * map.get(start).get(cha), visited);
+                return next;
+            }
+        }
+    }
+   
+    for (let query of queries) {
+        const [cha1, cha2] = query;
+        const visited = new Set();
+
+        if (map.has(cha1) && map.has(cha2)) {
+            if (cha2 === cha1) { answer.push(1); }
+            else if (map.get(cha1).has(cha2)) {
+                answer.push(map.get(cha1).get(cha2));
+            }
+            // map에 나눗셈 결과가 없으면 dfs 시작
+            else answer.push(dfs(cha1, cha2, 1, visited));
+        }
+        else answer.push(-1);
+    }
+
+    return answer;
+};
