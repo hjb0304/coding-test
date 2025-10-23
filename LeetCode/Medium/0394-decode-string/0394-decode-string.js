915416c88bcd46ab7bb37c492247d75cbb74f0ca
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-10-23 23:10:26 +0900

    Time: 0 ms (100%), Space: 54.2 MB (35.79%) - LeetHub

--- LeetCode/Medium/0394-decode-string/0394-decode-string.js
+++ LeetCode/Medium/0394-decode-string/0394-decode-string.js
@@ -7,7 +7,7 @@ var decodeString = function (s) {
 
     for (i = 0; i < s.length; i++) {
         const item = s[i];
-        console.log(stack)
+
         // ]이면 [가 나올 때까지 스택에서 하나씩 빼서 숫자만큼 반복 -> 다시 스택에 넣기 
         if (item === ']') {
             let num = '';
@@ -24,7 +24,7 @@ var decodeString = function (s) {
                 num = cha + num;
                 cha = stack.pop();
             }
-            
+
             stack.push(cha)
             stack.push(str.repeat(Number(num)));
             continue;
