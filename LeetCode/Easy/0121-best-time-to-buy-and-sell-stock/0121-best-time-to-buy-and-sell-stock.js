a815cb5c75cfd88559daac8802313a74bdbce121
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-10-27 21:29:59 +0900

    Time: 6 ms (14.31%), Space: 65.8 MB (44.91%) - LeetHub

---
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let max = 0;
+    let min = Infinity;
+
+    for(let price of prices) {
+        if(price < min) min = price;
+        if(price > min) max = Math.max(max, price - min);
+    }
+
+    return max;
+};
