1788107b38626f283825b66ac1e051d6ea32fd3d
Heejung Bae <43438259+hjb0304@users.noreply.github.com>
2025-11-11 15:17:38 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Medium/0474-ones-and-zeroes/0474-ones-and-zeroes.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var findMaxForm = function(strs, m, n) {
+    // 각 0, 1의 개수로 만들 수 있는 부분집합의 길이
+    const dp = Array({length: m}, () => Array(n).fill(0));
+
+    for(let i = 0; i < strs.length; i++) {
+        for(let j = 0; j < strs.length; j++) {
+            
+        }
+    }
+};
